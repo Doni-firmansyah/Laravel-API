429afdff8ca23a067cb211fd60921e259c2e5433
DESKTOP-ETC9R9F\Doni Firmansyah <didonifirmansyah@gmail.com>
2022-03-09 14:47:46 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\BarangController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/barang',[BarangController::class,'index']);
+Route::get('/barang/{id}',[BarangController::class,'show']);
+Route::post('/barang/create',[BarangController::class,'store']);
+Route::put('/barang/{id}',[BarangController::class,'update']);
+Route::delete('/barang/{id}',[BarangController::class,'destroy']);
